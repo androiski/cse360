192d00c346e5ad976cc5d2659bbfee39e79ff246
Andrew Murza <androiski@gmail.com>
2019-02-21 22:14:33 -0700

    part one of cse360 assignment 2 created calculator.java with all methods and javadoc comment style

---
+++ cse360repo/Assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,75 @@
+/**
+ * @author Andrew Murza
+ * ID		499
+ * Assignment 2
+ * 
+ * Calculator.java contains definitions for a Calculator
+ * 
+ * Calculator.java is a class that adds, subtracts, multiplies
+ * or divides a total value and prints the changes the operator made 
+ * on the value over the course of the program
+ */
+
+package cse360assign2;
+
+public class Calculator {
+	
+	/**
+	 * {@link int} for holding the total int value
+	 */
+	private int total;
+	
+	/**
+	 * Default constructor for calculator class
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Displays total value of the operations done on total
+	 * @return the int total of calculator
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Method for performing an add to total
+	 * @param value is added to total
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * Method for performing a subtract to total
+	 * @param value is subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Method for performing a multiply to total
+	 * @param value is multiplied by the total
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Method for performing a division to total
+	 * @param value is used to divide total by value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Method to display the history of operations done on total
+	 * @return a string displaying the operations executed in succession 
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
